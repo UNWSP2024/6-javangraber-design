498bf3f1e58b503ec1058a65dbf5c7376c3e4ab2
javangraber-design <javangraber@gmail.com>
2026-02-24 23:30:55 -0600

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,15 +1,42 @@
-
+# Programmer: Javan Graber
+# Date: 2/24/2026
 # Program #1: Random Dice
-# Write a "randDice" function (with no input) that randomly chooses two numbers between 1 and 6 (inclusive) and then adds them (this is to simulate the rolling of 2 dice).  
+# Write a "randDice" function (with no input) that randomly chooses two numbers between 1 and 6 (inclusive) and then adds them (this is to simulate the rolling of 2 dice).
 # The dice sum will be the output of this function.
 
+# First import the random module
+import random
+
+# Create a function that generates two random numbers and finds their sum
 def randDice():
-    # Write your logic to generate 2 numbers between 1 and 6 here
+    # Generate the first number
+    first_number = random.randint(1, 6)
 
-    # Sum 2 numbers
+    # Generate the second number
+    second_number = random.randint(1, 6)
 
-    # return sum to calling function
+    # Find the sum of the two numbers
+    sum_numbers = first_number + second_number
 
-#########
-# Then write a mainline that calls the "randDice" function 100 times in a for loop.  
-# The mainline then prints the average of the 100 rolls, rounded to the nearest 0.01.
+    # Return the sum to the calling function
+    return sum_numbers
+
+# Create the main function that simulates 100 rolls of the dice and calculates the
+# average by calling for the randDice function and using the formula
+def main():
+    # Establish the total as starting at 0
+    total = 0
+
+    # Insert the for loop that calls the randDice function 100 times
+    for dice_roll in range(100):
+        sum_rolls = randDice()
+        total = total + sum_rolls
+
+    # Create the average formula
+    average = total / 100
+
+    # Print the average
+    print(f'The average of the 100 rolls is {average:.2f}')
+
+# Call the main function
+main()
